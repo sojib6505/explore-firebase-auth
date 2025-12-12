60c563bf3fd0ee0264f2a76f4b5fd13fd3842a9e
Sojib Islam <sojibislam6505@gmail.com>
2025-12-12 12:22:01 +0600

    Explore SignIn with Github

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,11 @@
-import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth"
+import { GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth"
 import { auth } from "../../firebase.init"
 import { useState } from "react"
 
 export default function Login() {
   const [user, setUser] = useState(null)
-  const provider = new GoogleAuthProvider()
+  const provider = new GoogleAuthProvider();
+  const GithubProvider = new GithubAuthProvider();
   const handleBtn = () => {
     signInWithPopup(auth, provider)
       .then(
@@ -24,15 +25,25 @@ export default function Login() {
      console.log(error)
     )
   }
+  const handleGithubSignIn = () => {
+    signInWithPopup(auth , GithubProvider).then((result)=> {
+      setUser(result.user)
+    })
+  }
   return (
     <div>
       <h3>Please Login</h3>
       
       
       {
-        user ? <button onClick={handleSignOut}>Sign Out</button>:<button onClick={handleBtn}>Sign In With Google</button>
-
+        user ? <button onClick={handleSignOut}>Sign Out</button>
+        :<button onClick={handleBtn}>Sign In With Google</button>
       }
+         {
+        user ? ''
+        :<button onClick={handleGithubSignIn}>Sign In With Github</button>
+      }
+      
       {
         user && <div>
           <h2>{user.displayName}</h2>
